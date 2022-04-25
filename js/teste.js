f29fd95c87de75d46b1de91a3db322d2ebd236be
Mateus Borges <mateusgaspio@gmail.com>
2022-04-25 03:23:47 -0400

    Pronto o Template

---
+++ js/teste.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a').forEach(link => {
+    const conteudo = document.getElementById('conteudo')
+    link.onclick = function(e){
+        e.preventDefault()
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
